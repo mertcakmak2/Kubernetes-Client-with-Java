c43aa735df0d259dc43056e6d76284cf36ff095a
mertcakmak2 <mertcakmak2@gmail.com>
2021-11-30 11:18:44 +0300

    k8s client

---
+++ src/main/java/com/example/k8sjava/controller/K8sController.java
@@ -0,0 +1,36 @@
+package com.example.k8sjava.controller;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.Config;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/k8s")
+@RequiredArgsConstructor
+public class K8sController {
+
+    @GetMapping("/list-pods")
+    public List<V1Pod> listAllPods() throws IOException, ApiException {
+        ApiClient client  = Config.defaultClient();
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+        V1PodList list = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        for (V1Pod item : list.getItems()) {
+            System.out.println(item.getMetadata().getName());
+        }
+        return list.getItems();
+    }
+
+}
---
+++ src/test/java/com/example/k8sjava/K8sjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8sjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
