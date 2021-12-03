2969fb6b14bc7c61c92ae695542643956d45ccaf
mertcakmak2 <mertcakmak2@gmail.com>
2021-12-03 11:11:53 +0300

    deployment modify

--- src/main/java/com/example/k8sjava/service/deployment/DeploymentServiceImpl.java
+++ src/main/java/com/example/k8sjava/service/deployment/DeploymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.k8sjava.service.deployment;
 
+import com.example.k8sjava.model.CreateDeploymentModel;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.springframework.stereotype.Service;
@@ -20,5 +22,39 @@ public class DeploymentServiceImpl implements DeploymentService{
             deployments.add(deployment.getMetadata().getName());
         }
         return deployments;
+    }
+
+    public String createDeployment(CreateDeploymentModel deploymentModel){
+
+        //Todo : null vermeyi dene  yada deployment yaml dosyasındaki container portu sil.
+        Deployment deployment = new DeploymentBuilder()
+                .withNewMetadata()
+                .withName(deploymentModel.getName())
+                .endMetadata()
+                .withNewSpec()
+                .withReplicas(deploymentModel.getReplicas())
+                .withNewTemplate()
+                .withNewMetadata()
+                .addToLabels(deploymentModel.getMetadataLabels())
+                .endMetadata()
+                .withNewSpec()
+                .addNewContainer()
+                .withCommand(deploymentModel.getCommand())
+                .withArgs(deploymentModel.getArgs())
+                .withName(deploymentModel.getContainerName())
+                .withImage(deploymentModel.getImage())
+                .addNewPort()
+                .withContainerPort(deploymentModel.getContainerPort())
+                .endPort()
+                .endContainer()
+                .endSpec()
+                .endTemplate()
+                .withNewSelector()
+                .addToMatchLabels(deploymentModel.getSelectorLabels())
+                .endSelector()
+                .endSpec()
+                .build();
+
+        return "deployment created";
     }
 }
